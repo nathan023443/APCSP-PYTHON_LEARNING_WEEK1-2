16af519e8a506a67c4b707ee271b81dbbb427317
Denis <ndenis@cps.k12.il.us>
2024-09-03 12:29:08 -0500

    first commit

---
+++ lesson1.py
@@ -0,0 +1,29 @@
+#this is the print statement 
+# print statement allows us to print out a statment
+# comments are not printed out by the computer
+# comments are used to help us understand the code
+
+# print("hello my name is nathan denis")
+# print("I am a student at john hanock college prep")
+# print(" I am learning python python")
+# creat input for the user 
+
+
+# name= input("what is your name?")
+# print("hello",name)
+
+# input allows us to take input from the user
+
+# age=input( "user, what is your age?")
+# print("you are", age, "years old")
+
+# favorite_color=input("what is your favorite color?")
+# print("your favorite color is", favorite_color)
+
+# favorite_food=input("what is your favorite food?")
+# print("your favorite food is",favorite_food)
+
+# concatanetion is the process of combining strings with variables 
+
+favorite_animal= input("what is your favorite animal?")
+print("your favorite animal is a "+ favorite_animal)
